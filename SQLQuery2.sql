fb51f5caf323e1da8d9869fa4dd19ebee4fa0b55
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-12-12 00:57:40 -0800

    Added UC4-EditExistingContactOrState

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -2,24 +2,3 @@
 CREATE DATABASE ADDRESS_BOOK_SERVICE;
 USE ADDRESS_BOOK_SERVICE;
 
-------UC2------
-----CREATING TABLE-----
-CREATE TABLE ADDRESS_BOOK_SERVICE
-(
-FirstName char(25),
-LastName char(25),
-City char(25),
-StateName char(25),
-Zip int,
-Phone varchar(25),
-Email varchar(25)
-);
-SELECT * FROM ADDRESS_BOOK_SERVICE;
- -----UC3-----
- ----INSERT CONTACT IN ADDRESSBOOK------
-INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Nikita','Bolkar','Aurangabad','Maharashtra','412109','9157842345','nikita2001@gmail.com');
-INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Ankita','Khairnar','Mumbai','Maharashtra','560085','954645345','ankitakhair@gmail.com');
-INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Sandesh','Nikam','Pune','Maharashtra','8375','76378434534','sandesh@gmail.com');
-INSERT INTO ADDRESS_BOOK_SERVICE VALUES('rahul','saner','nashik','Maharashtra','574895','09384903','snaehal@gmail.com');
-INSERT INTO ADDRESS_BOOK_SERVICE VALUES('Priyanka','sonawane','hyderabad','Telangana','455454','0938490334','priyanka@gmail.com');
-SELECT * FROM ADDRESS_BOOK_SERVICE;
